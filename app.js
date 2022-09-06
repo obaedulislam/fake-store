218a52a981af9536c13490faf519c61e4ff43c2a
obaedulislam <obaed.cse@gmail.com>
2022-09-06 19:16:01 +0600

    Set all categories on menu

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log('Hello')
+const loadAllProducts = async() => {
+    const url = `https://fakestoreapi.com/products`;
+    const res = await fetch(url);
+    const data = await res.json();
+    return data;
+}
+const setAllProductCategories = async() => {
+    const data = await loadAllProducts();
+    const categoryItem = document.getElementById('category-item');
+    const uniqueCategory = [];
+    for(let product of data){
+        const array = uniqueCategory.indexOf(product.category);
+        console.log(array);
+        if(uniqueCategory.indexOf(product.category) === -1){
+            uniqueCategory.push(product.category)
+            const listItem = document.createElement('li');
+            listItem.innerHTML =`
+                <a>${product.category}</a>
+            `;
+            categoryItem.appendChild(listItem);
+        }
+    }
+}
+setAllCategories();
