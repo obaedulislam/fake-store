0cfedd3e77df7a10cef1e75e4a2e2ae21337fcde
obaedulislam <obaed.cse@gmail.com>
2022-09-06 23:26:35 +0600

    All product shown successfull

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ const setAllProductCategories = async() => {
     const uniqueCategory = [];
     for(let product of data){
         const array = uniqueCategory.indexOf(product.category);
-        console.log(array);
         if(uniqueCategory.indexOf(product.category) === -1){
             uniqueCategory.push(product.category)
             const listItem = document.createElement('li');
@@ -21,4 +20,38 @@ const setAllProductCategories = async() => {
         }
     }
 }
-setAllCategories();
+setAllProductCategories();
+
+//Search Product
+const searchProduct = document.getElementById('search-field');
+searchProduct.addEventListener("keypress", async(event) => {
+    if(event.key === "Enter"){
+        const searchValue = searchProduct.value;
+        searchProduct.value = '';
+        const allProducts = await loadAllProducts();
+        const foundProducts = allProducts.filter(product => product.category.includes(searchValue));
+        if(foundProducts === 0){
+            
+        }
+        const productContainer = document.getElementById('product-container');
+        productContainer.textContent = '';
+        foundProducts.forEach(product => {
+
+            const{category, title, image, price, description} = product;
+            const div = document.createElement('div');
+            div.innerHTML = `
+            <div class="card card-compact w-full bg-base-100 shadow-xl">
+                <figure><img class="h-72 w-full" src="${image}" alt="Shoes" /></figure>
+                <div class="card-body">
+                    <h2 class="card-title">${title.length > 20 ? title.slice(0, 20)+'...' : title}</h2>
+                    <p>${description.slice(0, 100)}</p>
+                    <div class="card-actions justify-end">
+                        <button class="btn btn-primary">Show Details</button>
+                    </div>
+                </div>
+            </div>
+            `;
+            productContainer.appendChild(div);
+        })
+    }
+})
